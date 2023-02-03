ee36a8162a2aa307eb7407e30e403178539c63a7
Dustin Stacy <dustin.stacy@outlook.com>
2023-02-03 00:28:10 -0500

    Add more Landing/Login page styling

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './Button/Button'
 export { default as Footer } from './Footer/Footer'
 export { default as NavBar } from './NavBar/NavBar'
+export { default as TextInput } from './TextInput/TextInput'
--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Button } from '../../components'
+import { logo } from '../../assets'
+import { Button, TextInput } from '../../components'
 import './LandingPage.scss'
 
 const LandingPage = () => {
 	return (
 		<div className='landing page'>
 			<div className='content'>
-				<div></div>
-				<Button className='button' label='Create Account' />
+				<img className='logo__large' src={logo} alt='logo' />
+				<form className='form'>
+					<TextInput label='Username' />
+					<TextInput label='Email' />
+					<TextInput label='Password' />
+					<TextInput label='Confirm Password' />
+				</form>
+				<Button id='button' label='Create Account' />
 			</div>
 		</div>
 	)

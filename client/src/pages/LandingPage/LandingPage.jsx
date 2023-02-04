e47ac5509012f8822674ebdbe27df44122426603
Dustin Stacy <dustin.stacy@outlook.com>
2023-02-03 23:53:25 -0500

    Add password visibility toggle

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { NavLink, useNavigate } from 'react-router-dom'
+
 import { useGlobalContext } from '../../context/GlobalContext'
 import { logo } from '../../assets'
 import { Button, TextInput } from '../../components'
@@ -12,8 +13,8 @@ const LandingPage = ({ login, register }) => {
 	const [email, setEmail] = useState('')
 	const [password, setPassword] = useState('')
 	const [confirmPassword, setConfirmPassword] = useState('')
-	const [passwordVisisble, setPasswordVisisble] = useState(false)
-	const [confirmPasswordVisisble, setConfirmPasswordVisisble] = useState(false)
+	const [passwordVisible, setPasswordVisible] = useState(false)
+	const [confirmpasswordVisible, setConfirmpasswordVisible] = useState(false)
 	const [loading, setLoading] = useState(false)
 	const [errors, setErrors] = useState({})
 
@@ -42,7 +43,7 @@ const LandingPage = ({ login, register }) => {
 	return (
 		<div className='landing page'>
 			{!login && !register && (
-				<div className='content'>
+				<div className='auth'>
 					<img className='logo__large' src={logo} alt='logo' />
 					<Button
 						id='button'
@@ -54,15 +55,16 @@ const LandingPage = ({ login, register }) => {
 			)}
 
 			{(register || login) && (
-				<div className='content'>
+				<div className='auth'>
 					<img className='logo__medium' src={logo} alt='logo' />
-					<form className='form'>
+					<form className='auth__form'>
 						<TextInput
 							label='Username'
 							value={username}
 							setState={setUsername}
 							loading={loading}
 						/>
+						{errors.name && <p className='auth__error'>{errors.username}</p>}
 						{register && (
 							<TextInput
 								label='Email'
@@ -71,28 +73,39 @@ const LandingPage = ({ login, register }) => {
 								loading={loading}
 							/>
 						)}
+						{errors.email && <p className='auth__error'>{errors.email}</p>}
 						<TextInput
 							label='Password'
 							value={password}
 							setState={setPassword}
 							loading={loading}
+							passwordVisibility={passwordVisible}
+							setPasswordVisibility={setPasswordVisible}
 						/>
+						{errors.password && (
+							<p className='auth__error'>{errors.password}</p>
+						)}
 						{register && (
 							<TextInput
 								label='Confirm Password'
 								value={confirmPassword}
 								setState={setConfirmPassword}
 								loading={loading}
+								passwordVisibility={confirmpasswordVisible}
+								setPasswordVisibility={setConfirmpasswordVisible}
 							/>
+						)}
+						{errors.confirmPassword && (
+							<p className='auth__error'>{errors.confirmPassword}</p>
 						)}
 
 						{register ? (
-							<div className='form__footer'>
+							<div className='auth__footer'>
 								<span>Already a member? </span>
 								<NavLink to='/login'>Login</NavLink>
 							</div>
 						) : (
-							<div className='form__footer'>
+							<div className='auth__footer'>
 								<span>Not a member? </span>
 								<NavLink to='/register'>Sign up</NavLink>
 							</div>

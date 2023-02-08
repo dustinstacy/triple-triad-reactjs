163dd327d85445daa84041099d720011b182c5e8
Dustin Stacy <dustin.stacy@outlook.com>
2023-02-08 10:42:51 -0500

    Add Collection layout and MatchSetup page.
    minor clean up

--- client/src/pages/Arcaneum/Arcaneum.jsx
+++ client/src/pages/Arcaneum/Arcaneum.jsx
@@ -8,24 +8,24 @@ const Arcaneum = () => {
 		<div className='arcaneum page'>
 			<div className='menu'>
 				<div className='menu__top'>
-					<div className='menu__card'>
+					<div className='menu__card box'>
 						<Button label='Collection' type='link' path='collection' />
 					</div>
-					<div className='menu__card disabled'>
+					<div className='menu__card box disabled'>
 						<Button label='Deck' />
 					</div>
 				</div>
 				<div className='menu__middle'>
-					<div className='menu__card disabled'>
+					<div className='menu__card box disabled'>
 						<Button label='Packs' type='link' path='arcaneum' />
 					</div>
 					<img className='menu__spell' src={spellCircle} alt='spell circle' />
-					<div className='menu__card disabled'>
+					<div className='menu__card box disabled'>
 						<Button label='Fusion' type='link' path='arcaneum' />
 					</div>
 				</div>
 				<div className='menu__bottom'>
-					<div className='menu__card disabled'>
+					<div className='menu__card box disabled'>
 						<Button label='Market' type='link' path='arcaneum' />
 					</div>
 				</div>

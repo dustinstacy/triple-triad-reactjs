93d27bcc65e9eac4dc80343a8b9eb5726109a564
Dustin Stacy <dustin.stacy@outlook.com>
2023-02-13 23:33:21 -0500

    Add final Deck filters, Deck Page Header bar, Collection page styling

--- client/src/pages/Collection/Collection.jsx
+++ client/src/pages/Collection/Collection.jsx
@@ -5,6 +5,8 @@ import './Collection.scss'
 const Collection = () => {
 	const { allCards, userCards, getUserCards } = useGlobalContext()
 	const sortedCards = allCards.sort((a, b) => a.number - b.number)
+	const cardNames = userCards.map((card) => card.name)
+	const uniqueCards = [...new Set(cardNames)]
 
 	useEffect(() => {
 		getUserCards
@@ -13,25 +15,37 @@ const Collection = () => {
 	return (
 		<div className='collection page'>
 			<div className='background' />
-			<div className='filters box'></div>
+
 			<div className='list'>
-				{sortedCards.map((card) =>
-					userCards.find((userCard) => userCard.name === card.name) ? (
-						<div key={card.name} className='display'>
-							<div className='card'>
-								<img className='card__image' src={card.image} alt='owl' />
+				<h1 className='header box'>
+					Collection
+					<span>
+						Total Found : {uniqueCards.reduce((total) => total + 1, 0)} / 281
+					</span>
+				</h1>
+				<div className='container'>
+					{sortedCards.map((card) =>
+						userCards.find((userCard) => userCard.name === card.name) ? (
+							<div key={card.name} className='display'>
+								<div className='card'>
+									<img className='card__image' src={card.image} alt='owl' />
+								</div>
+								<div className='info'>
+									<p>{card.number} / 281</p>
+									<p>{card.name}</p>
+								</div>
 							</div>
-							<p>{card.number} / 281</p>
-							<p>{card.name}</p>
-						</div>
-					) : (
-						<div key={card.name} className='display'>
-							<div className='card disabled' />
-							<p>??? / 281</p>
-							<p>????????</p>
-						</div>
-					)
-				)}
+						) : (
+							<div key={card.name} className='display'>
+								<div className='card disabled' />
+								<div className='info'>
+									<p>??? / 281</p>
+									<p>????????</p>
+								</div>
+							</div>
+						)
+					)}
+				</div>
 			</div>
 		</div>
 	)

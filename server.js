ba6556bdb433a9f6efb41bd5570a080d68a7eac5
Dustin Stacy <dustin.stacy@outlook.com>
2023-02-28 16:29:32 -0500

    vite deployment debugging

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/cards', cardsRoute)
 app.use('/api/collection', collectionRoute)
 app.use('/api/deck', deckRoute)
 app.use('/api/profile', profileRoute)
-app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.static(path.resolve(__dirname, './client/dist')))
 
 app.get('*', (req, res) => {
 	res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'))

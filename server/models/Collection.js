5292672cfebcc6ff0d43368f653d0b16645f8b87
Dustin Stacy <dustin.stacy@outlook.com>
2023-05-02 14:34:51 -0400

    Indent spacing fix test

--- server/models/Collection.js
+++ server/models/Collection.js
@@ -2,45 +2,45 @@ import { Schema, model } from 'mongoose'
 
 // user obtained cards with added values and favorite option
 const CollectionSchema = new Schema(
-	{
-		user: {
-			type: Schema.Types.ObjectId,
-			ref: 'User',
-		},
-		number: {
-			type: String,
-			required: true,
-		},
-		name: {
-			type: String,
-			required: true,
-		},
-		rarity: {
-			type: String,
-			required: true,
-		},
-		element: {
-			type: String,
-			required: true,
-		},
-		image: {
-			type: String,
-			required: true,
-		},
-		values: [
-			{
-				type: String,
-				required: true,
-			},
-		],
-		selected: {
-			type: Boolean,
-			default: false,
-		},
-	},
-	{
-		timestamp: true,
-	}
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        number: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        rarity: {
+            type: String,
+            required: true,
+        },
+        element: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        values: [
+            {
+                type: String,
+                required: true,
+            },
+        ],
+        selected: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamp: true,
+    }
 )
 
 const Collection = model('Collection', CollectionSchema)

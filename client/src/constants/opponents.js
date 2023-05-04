ad89f2ea1812b136b180fc48b094e29abac16952
Dustin Stacy <dustin.stacy@outlook.com>
2023-05-04 18:54:37 -0400

    constants update

--- client/src/constants/opponents.js
+++ client/src/constants/opponents.js
@@ -1,7 +1,44 @@
-export const hades = {
-    name: 'Hades',
-    image: 'https://res.cloudinary.com/dhsflmylz/image/upload/v1677541930/triple-triad/cpu/hades_p0t4y6.png',
-    rank: 1,
-    coinReward: 25,
+export const goblin = {
+    name: 'Goblin',
+    image: 'https://res.cloudinary.com/dhsflmylz/image/upload/v1676139372/goblin_pet_jgqg2v.png',
+    level: 1,
+    coinReward: 10,
+    xpReward: 5,
+    card: '17',
+}
+
+export const aquos = {
+    name: 'Aquos',
+    image: 'https://res.cloudinary.com/dhsflmylz/image/upload/v1676488707/aquos_xdn1jb.png',
+    level: 2,
+    coinReward: 20,
+    xpReward: 5,
+    card: '128',
+}
+
+export const clockworkTarantula = {
+    name: 'Clockwork Tarantula',
+    image: 'https://res.cloudinary.com/dhsflmylz/image/upload/v1676490236/clockwork_spider_mgyjsj.png',
+    level: 3,
+    coinReward: 30,
     xpReward: 10,
+    card: '161',
+}
+
+export const greaterMimic = {
+    name: 'Greater Mimic',
+    image: 'https://res.cloudinary.com/dhsflmylz/image/upload/v1676490583/greater_mimic_nfgvqc.png',
+    level: 4,
+    coinReward: 40,
+    xpReward: 10,
+    card: '177',
+}
+
+export const asmodeus = {
+    name: 'Asmodeus',
+    image: 'https://res.cloudinary.com/dhsflmylz/image/upload/v1675970328/asmodeus_bkpodp.png',
+    level: 3,
+    coinReward: 50,
+    xpReward: 15,
+    card: '201',
 }

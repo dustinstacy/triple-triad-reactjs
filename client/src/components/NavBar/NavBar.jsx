c33d441b1b95ab3a4fc854a3399c61245bdb6ea2
Dustin Stacy <dustin.stacy@outlook.com>
2023-05-06 19:45:51 -0400

    Add NavBar refactor

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,147 +1,180 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { useNavigate, NavLink } from 'react-router-dom'
 import { MdOutlineClose, MdLogout, MdMenu } from 'react-icons/md'
+
 import axios from 'axios'
 import { motion } from 'framer-motion'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 import { useGlobalContext } from '../../context/GlobalContext'
+import { handleToggle } from '../../utils/handleToggle'
 
 import { logo } from '../../assets/logos'
-import { coin } from '../../assets/icons'
+import { coinImage } from '../../assets/icons'
 import { levels } from '../../constants/levels'
 import { navlinks } from '../../constants/navlinks'
 
 import './NavBar.scss'
 
-const BurgerMenu = () => {
-    const [isOpen, setIsOpen] = useState(false)
+const Links = ({ menu, onClick }) => {
+    const linkItems = useMemo(
+        () =>
+            navlinks.map((link) => (
+                <NavLink
+                    className={`${menu}-link center`}
+                    key={link.name}
+                    to={link.path}
+                    onClick={onClick}
+                >
+                    {link.image}
+                    <p>{link.name}</p>
+                </NavLink>
+            )),
+        [menu]
+    )
 
-    const handleToggle = () => {
-        setIsOpen((isOpen) => !isOpen)
-    }
+    return <div className={`${menu}-links`}>{linkItems}</div>
+}
+
+const BurgerMenu = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const isSmallScreen = useMediaQuery('(min-width:600px)')
+
+    useEffect(() => {
+        return () => {
+            setIsMenuOpen(false)
+        }
+    }, [, isSmallScreen])
 
     return (
         <div className='burger-menu'>
-            {!isOpen ? (
-                <MdMenu onClick={() => handleToggle()} />
+            {!isMenuOpen ? (
+                <MdMenu onClick={() => handleToggle(setIsMenuOpen)} />
             ) : (
-                <MdOutlineClose onClick={() => handleToggle()} />
+                <MdOutlineClose onClick={() => handleToggle(setIsMenuOpen)} />
             )}
             <motion.div
                 className='menu'
                 initial={{ width: 0 }}
                 animate={
-                    window.innerWidth > 600
-                        ? { width: isOpen ? '25vw' : '0' }
-                        : { width: isOpen ? '50vw' : '0' }
+                    isSmallScreen
+                        ? { width: isMenuOpen ? '30vw' : '0' }
+                        : { width: isMenuOpen ? '60vw' : '0' }
                 }
                 transition={{
                     duration: 0.3,
                     ease: 'easeInOut',
                 }}
             >
-                <div className='links'>
-                    {navlinks.map((link) => (
-                        <NavLink
-                            key={link.name}
-                            to={link.path}
-                            onClick={() => setIsOpen(false)}
-                        >
-                            {link.name}
-                        </NavLink>
-                    ))}
-                </div>
+                <Links
+                    menu='burger-menu'
+                    onClick={() => handleToggle(setIsMenuOpen)}
+                />
             </motion.div>
         </div>
     )
 }
 
-const Links = () => {
+const UserInventory = ({ user }) => {
+    const { coin } = user ?? {}
     return (
-        <div className='links'>
-            {navlinks.map((link) => (
-                <NavLink className='link center' key={link.name} to={link.path}>
-                    {link.image}
-                    <p>{link.name}</p>
-                </NavLink>
-            ))}
+        <div className='user-inventory'>
+            <p className='coin center'>
+                {coin} <img src={coinImage} alt='coin' />
+            </p>
         </div>
     )
 }
 
-const User = () => {
-    const { user, getCurrentUser, logout } = useGlobalContext()
-    const navigate = useNavigate()
-    const [toggle, setToggle] = useState(false)
-    const userNextLevel = levels[user?.level]
+const UserInfo = ({ user }) => {
+    const { getCurrentUser } = useGlobalContext()
+    const { username, xp, level } = user ?? {}
+    const userNextLevel = levels[level]
+
     const xpPercentage = () => {
-        const percentage = (user?.xp / userNextLevel) * 100 + '%'
-        return percentage
+        return `${(xp / userNextLevel) * 100}%`
+    }
+
+    const handleLevelUp = () => {
+        axios
+            .put('/api/profile', { level: level + 1 })
+            .then(() => getCurrentUser())
     }
 
     useEffect(() => {
-        if (user?.xp >= userNextLevel) {
-            axios
-                .put('/api/profile', {
-                    level: user.level + 1,
-                })
-                .then(() => {
-                    getCurrentUser()
-                })
+        if (xp >= userNextLevel) {
+            handleLevelUp()
         }
-    }, [user, getCurrentUser, userNextLevel])
+    }, [xp, userNextLevel])
+
+    return (
+        <div className='user-info'>
+            <h2>{username}</h2>
+            <div className='progressBar'>
+                <div
+                    className='progressBar__inner'
+                    style={{ width: xpPercentage() }}
+                ></div>
+            </div>
+            <span className='xp'>
+                XP {xp} / {userNextLevel}
+            </span>
+        </div>
+    )
+}
+
+const UserImage = ({ user }) => {
+    const { image, level } = user ?? {}
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <div className='user-image'>
+            <div className='image-inner '>
+                <img
+                    src={image}
+                    alt='user image'
+                    onClick={() => handleToggle(setIsOpen)}
+                />
+                <p className='level box'>LVL {level}</p>
+                <UserMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+            </div>
+        </div>
+    )
+}
+
+const UserMenu = ({ isOpen, setIsOpen }) => {
+    const { logout } = useGlobalContext()
+
+    const navigate = useNavigate()
 
     const handleLogout = () => {
         logout().then(() => {
-            setToggle(false)
+            handleToggle(setIsOpen)
             navigate('/')
         })
     }
 
     return (
+        isOpen && (
+            <div className='user-menu box'>
+                <NavLink to='/account' onClick={() => handleToggle(setIsOpen)}>
+                    Account
+                </NavLink>
+                <a className='user-link center' onClick={() => handleLogout()}>
+                    Logout <MdLogout />
+                </a>
+            </div>
+        )
+    )
+}
+
+const UserSection = ({ user }) => {
+    return (
         <div className='user'>
             <hr />
-
-            <div className='user__inventory'>
-                <p className='coin center'>
-                    {user?.coin} <img src={coin} alt='coin' />
-                </p>
-            </div>
-
-            <div className='user__info'>
-                <h2>{user?.username}</h2>
-                <div className='progressBar'>
-                    <div
-                        className='progressBar__inner'
-                        style={{ width: xpPercentage() }}
-                    ></div>
-                </div>
-                <span className='xp'>
-                    XP {user?.xp} / {userNextLevel}
-                </span>
-            </div>
-
-            <div className='user__image'>
-                <div className='image-inner '>
-                    <img
-                        src={user?.image}
-                        alt='user image'
-                        onClick={() => setToggle((current) => !current)}
-                    />
-                    <p className='level box'>LVL {user?.level}</p>
-                    {toggle && (
-                        <div className='user-menu box'>
-                            <a href='/account'>Account</a>
-                            <a
-                                className='user-link center'
-                                onClick={() => handleLogout()}
-                            >
-                                Logout <MdLogout />
-                            </a>
-                        </div>
-                    )}
-                </div>
-            </div>
+            <UserInventory user={user} />
+            <UserInfo user={user} />
+            <UserImage user={user} />
         </div>
     )
 }
@@ -159,15 +192,13 @@ const NavBar = ({ landing }) => {
                 className='navbar__logo'
                 onClick={() => navigate('/home')}
             />
-            <Links />
+            <Links menu='navbar' />
             {user ? (
-                <User />
-            ) : (
-                !landing && (
-                    <a className='navbar__login box' href='/'>
-                        Login
-                    </a>
-                )
+                <UserSection user={user} />
+            ) : landing ? null : (
+                <NavLink className='navbar__login box' to='/'>
+                    Login
+                </NavLink>
             )}
 
             <hr className='gold-border-bottom' />

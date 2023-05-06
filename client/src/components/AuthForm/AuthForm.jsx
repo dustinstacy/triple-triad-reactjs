4a20ce9ce320852529a60c6ad859946cf9358c54
Dustin Stacy <dustin.stacy@outlook.com>
2023-05-05 23:47:57 -0400

    Add component refactoring

--- client/src/components/AuthForm/AuthForm.jsx
+++ client/src/components/AuthForm/AuthForm.jsx
@@ -8,11 +8,12 @@ import './AuthForm.scss'
 
 const AuthForm = ({ register }) => {
     const { getCurrentUser } = useGlobalContext()
-
-    const [username, setUsername] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setConfirmPassword] = useState('')
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+    })
     const [loading, setLoading] = useState(false)
     const [errors, setErrors] = useState({})
 
@@ -22,31 +23,26 @@ const AuthForm = ({ register }) => {
         e.preventDefault()
         setLoading(true)
 
-        let data = {}
-        if (register) {
-            data = {
-                username,
-                email,
-                password,
-                confirmPassword,
+        const { username, email, password, confirmPassword } = inputs
+
+        const data = register
+            ? { username, email, password, confirmPassword }
+            : { username, password }
+
+        try {
+            await axios.post(
+                register ? '/api/auth/register' : '/api/auth/login',
+                data
+            )
+
+            await getCurrentUser().then(navigate('/home'))
+        } catch (error) {
+            if (error?.response?.data) {
+                setErrors(error.response.data)
             }
-        } else {
-            data = {
-                username,
-                password,
-            }
+        } finally {
+            setLoading(false)
         }
-        await axios
-            .post(register ? '/api/auth/register' : '/api/auth/login', data)
-            .then(() => {
-                getCurrentUser(), navigate('/home')
-            })
-            .catch((error) => {
-                setLoading(false)
-                if (error?.response?.data) {
-                    setErrors(error.response.data)
-                }
-            })
     }
 
     const handleKeyDown = (e) => {
@@ -56,10 +52,12 @@ const AuthForm = ({ register }) => {
     }
 
     const reset = () => {
-        setUsername('')
-        setEmail('')
-        setPassword('')
-        setConfirmPassword('')
+        setInputs({
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+        })
         setLoading(false)
         setErrors({})
     }
@@ -73,8 +71,13 @@ const AuthForm = ({ register }) => {
             <form className='form center' onKeyDown={(e) => handleKeyDown(e)}>
                 <TextInput
                     label='Username'
-                    value={username}
-                    setState={setUsername}
+                    value={inputs.username}
+                    setValue={(value) =>
+                        setInputs((prevState) => ({
+                            ...prevState,
+                            username: value,
+                        }))
+                    }
                     loading={loading}
                     autofocus
                 />
@@ -82,50 +85,73 @@ const AuthForm = ({ register }) => {
                     <p className='form__error'>{errors.username}</p>
                 )}
                 {register && (
-                    <TextInput
-                        label='Email'
-                        value={email}
-                        setState={setEmail}
-                        loading={loading}
-                    />
+                    <>
+                        <TextInput
+                            label='Email'
+                            value={inputs.email}
+                            setValue={(value) =>
+                                setInputs((prevState) => ({
+                                    ...prevState,
+                                    email: value,
+                                }))
+                            }
+                            loading={loading}
+                        />
+                        {errors.email && (
+                            <p className='form__error'>{errors.email}</p>
+                        )}
+                    </>
                 )}
-                {errors.email && <p className='form__error'>{errors.email}</p>}
                 <TextInput
                     label='Password'
-                    value={password}
-                    setState={setPassword}
+                    value={inputs.password}
+                    setValue={(value) =>
+                        setInputs((prevState) => ({
+                            ...prevState,
+                            password: value,
+                        }))
+                    }
                     loading={loading}
                 />
+
                 {errors.password && (
                     <p className='form__error'>{errors.password}</p>
                 )}
                 {register && (
-                    <TextInput
-                        label='Confirm Password'
-                        value={confirmPassword}
-                        setState={setConfirmPassword}
-                        loading={loading}
-                    />
-                )}
-                {errors.confirmPassword && (
-                    <p className='form__error'>{errors.confirmPassword}</p>
+                    <>
+                        {' '}
+                        <TextInput
+                            label='Confirm Password'
+                            value={inputs.confirmPassword}
+                            setValue={(value) =>
+                                setInputs((prevState) => ({
+                                    ...prevState,
+                                    confirmPassword: value,
+                                }))
+                            }
+                            loading={loading}
+                        />
+                        {errors.confirmPassword && (
+                            <p className='form__error'>
+                                {errors.confirmPassword}
+                            </p>
+                        )}
+                    </>
                 )}
 
                 {Object.keys(errors).length > 0 && !register && (
                     <p className='form__error'>Nope. Try Again.</p>
                 )}
-
-                {register ? (
-                    <div className='form__footer'>
-                        <span>Already have an account? </span>
-                        <NavLink to='/'>Login</NavLink>
-                    </div>
-                ) : (
-                    <div className='form__footer'>
-                        <span>Need an account? </span>
-                        <NavLink to='/register'>Sign up</NavLink>
-                    </div>
-                )}
+                <div className='form__footer'>
+                    <span>
+                        {register
+                            ? 'Already have an account? '
+                            : 'Need an account? '}
+                    </span>
+                    <NavLink to={register ? '/' : '/register'}>
+                        {register ? 'Login' : 'Sign up'}
+                    </NavLink>
+                </div>
             </form>
             <Button
                 label='Submit'
--- server/models/User.js
+++ server/models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new Schema(
         image: {
             type: String,
             default:
-                'https://res.cloudinary.com/dhsflmylz/image/upload/v1677514304/triple-triad/cards/defaultPlayer_unuhf2.png',
+                'https://res.cloudinary.com/dhsflmylz/image/upload/v1677514304/triple-triad/backgrounds/defaultPlayer_unuhf2.png',
         },
         coin: {
             type: Number,

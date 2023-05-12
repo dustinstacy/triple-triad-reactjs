7e62e17d9a46fbbd4af451abfb0c01cf0dae206f
Dustin Stacy <dustin.stacy@outlook.com>
2023-05-12 12:58:14 -0400

    Add Market page Quantity Selector component, Purchase bar element, and item selection

--- client/src/pages/Market/Market.jsx
+++ client/src/pages/Market/Market.jsx
@@ -1,18 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import { useGlobalContext } from '../../context/GlobalContext'
+import { Button } from '../../components'
 import { marketItems } from '../../constants/marketItems'
 import { coinImage } from '../../assets/icons'
 
 import './Market.scss'
 
+const QuantitySelector = ({
+    chosenItem,
+    chosenQuantity,
+    setChosenQuantity,
+}) => {
+    const handleQuantityChange = (quantity) => {
+        setChosenQuantity(quantity)
+    }
+
+    const quantityOptions = chosenItem.quantities
+
+    return (
+        <div className='quantity-selector'>
+            <h2>ChOOse Quantity :</h2>
+            <div className='quantity-buttons'>
+                {quantityOptions.map((quantity) => (
+                    <button
+                        key={quantity}
+                        onClick={() => handleQuantityChange(quantity)}
+                        className={`quantity-button ${
+                            chosenQuantity === quantity ? 'chosen' : ''
+                        }`}
+                    >
+                        {quantity}
+                    </button>
+                ))}
+            </div>
+        </div>
+    )
+}
+
 const Market = () => {
     const { getCurrentUser } = useGlobalContext()
     const [chosenItem, setChosenItem] = useState(marketItems[0])
+    const [chosenQuantity, setChosenQuantity] = useState(1)
 
     useEffect(() => {
         getCurrentUser()
-        console.log(chosenItem)
     }, [])
+
+    useEffect(() => {
+        console.log(chosenItem)
+    }, [chosenItem])
+
     return (
         <div className='market page center'>
             <div className='market-menu box'>
@@ -22,10 +59,11 @@ const Market = () => {
                 </div>
                 <div className='menu-body'>
                     <div className='menu-items'>
-                        {marketItems.map((item) => (
+                        {marketItems.map((item, i) => (
                             <div
                                 key={'menu-' + item.name}
                                 className='menu-item'
+                                onClick={() => setChosenItem(marketItems[i])}
                             >
                                 {item.name}
                                 <div className='menu-item-price center'>
@@ -52,6 +90,21 @@ const Market = () => {
                                     {chosenItem.details}
                                 </p>
                             </div>
+                        </div>
+                        <QuantitySelector
+                            chosenItem={chosenItem}
+                            chosenQuantity={chosenQuantity}
+                            setChosenQuantity={setChosenQuantity}
+                        />
+                        <div className='purchase-bar box'>
+                            <div className='total'>
+                                Total :
+                                <div className='amount center'>
+                                    {chosenItem.price}
+                                    <img src={coinImage} alt='coin' />
+                                </div>
+                            </div>
+                            <Button label='Purchase' />
                         </div>
                     </div>
                 </div>

885cad8f9979fb4c3d91a445cc1b99199e60acc2
Dustin Stacy <dustin.stacy@outlook.com>
2023-05-12 11:53:22 -0400

    Add initial marketItems data

---
+++ client/src/constants/marketItems.js
@@ -0,0 +1,29 @@
+export const marketItems = [
+    {
+        name: 'Common Discovery',
+        details: 'Enter Item Description here',
+        type: 'discovery',
+        contents: { count: 1, chance: 'common' },
+        price: 150,
+        image: url('../assets/packs/pack1.png'),
+        level: 1,
+    },
+    {
+        name: 'Uncommon Discovery',
+        details: 'Enter Item Description here',
+        type: 'discovery',
+        contents: { count: 1, chance: 'uncommon' },
+        price: 400,
+        image: url('../assets/packs/pack2.png'),
+        level: 1,
+    },
+    {
+        name: 'Rare Discovery',
+        details: 'Enter Item Description here',
+        type: 'discovery',
+        contents: { count: 1, chance: 'rare' },
+        price: 1000,
+        image: url('../assets/packs/pack3.png'),
+        level: 1,
+    },
+]

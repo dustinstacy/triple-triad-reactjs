4df8b0d105b8b5ed60bac7cebb3a59359b3df07e
Dustin Stacy <dustin.stacy@outlook.com>
2023-05-14 11:43:46 -0400

    Add Discovery page carousel

--- client/src/pages/Discovery/Discovery.jsx
+++ client/src/pages/Discovery/Discovery.jsx
@@ -1,8 +1,56 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '../../components'
 import './Discovery.scss'
 import { uniqueItemsFilter } from '../../utils/uniqueItemsFilter'
 import { useGlobalContext } from '../../context/GlobalContext'
+import { BiLeftArrow, BiRightArrow } from 'react-icons/bi'
+
+const Carousel = ({ items, userInventory }) => {
+    const [currentItemIndex, setCurrentItemIndex] = useState(0)
+    const [slideDirection, setslideDirection] = useState('')
+
+    const handlePrevious = () => {
+        setslideDirection('previous')
+        setTimeout(() => {
+            setCurrentItemIndex((index) =>
+                index === 0 ? items.length - 1 : index - 1
+            )
+            setslideDirection('')
+        }, 300)
+    }
+
+    const handleNext = () => {
+        setslideDirection('next')
+        setTimeout(() => {
+            setCurrentItemIndex((index) =>
+                index === items.length - 1 ? 0 : index + 1
+            )
+            setslideDirection('')
+        }, 300)
+    }
+
+    const currentItem = items[currentItemIndex]
+    const previousItem =
+        items[(currentItemIndex + items.length - 1) % items.length]
+    const nextItem = items[(currentItemIndex + 1) % items.length]
+
+    return (
+        <div className='carousel'>
+            <BiLeftArrow className='arrow' onClick={handlePrevious} />
+            <div className={`carousel-item previous ${slideDirection}`}>
+                <img src={previousItem.image} alt='' />
+            </div>
+            <div className={`carousel-item current ${slideDirection}`}>
+                <img src={currentItem.image} alt={currentItem.name} />
+                <h3>{currentItem.name}</h3>
+            </div>
+            <div className={`carousel-item next ${slideDirection}`}>
+                <img src={nextItem.image} alt='' />
+            </div>
+            <BiRightArrow className='arrow' onClick={handleNext} />
+        </div>
+    )
+}
 
 const Discovery = () => {
     const { getCurrentUser, user } = useGlobalContext()
@@ -24,15 +72,12 @@ const Discovery = () => {
                     <hr />
                 </div>
                 <div className='user-discoveries center'>
-                    {uniqueDiscoveries?.map((discovery) => {
-                        return (
-                            <img
-                                key={discovery.name}
-                                src={discovery.image}
-                                alt={discovery.name}
-                            />
-                        )
-                    })}
+                    {uniqueDiscoveries.length && (
+                        <Carousel
+                            items={uniqueDiscoveries}
+                            userInventory={userDiscoveries}
+                        />
+                    )}
                 </div>
                 <div className='panel-footer center'>
                     <Button label='Make DiScoVery' />

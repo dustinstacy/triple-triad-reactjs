4feaf52c69f28902f5128116ae3520a8d5577c4c
Dustin Stacy <dustin.stacy@outlook.com>
2023-05-25 12:56:19 -0400

    Add final ProductTour modal

--- client/src/pages/Rules/Rules.jsx
+++ client/src/pages/Rules/Rules.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import { useGlobalContext } from '../../context/GlobalContext'
+import { ProductTour } from '../../components'
+
 import './Rules.scss'
 
 const Rules = () => {
+    const { user } = useGlobalContext()
+    const stage = user?.onboardingStage
+
     return (
         <div className='rules page'>
+            {stage === 3 && <ProductTour step={5} />}
             <div className='panel'>
                 <h1>HoW To PLay</h1>
                 <p>- This is how you play</p>

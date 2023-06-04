71d6342056d4ee2402694691732a371ee4775118
Dustin Stacy <dustin.stacy@outlook.com>
2023-06-04 19:24:01 -0400

    Add How To Play first draft

---
+++ client/src/constants/howToPlay.js
@@ -0,0 +1,59 @@
+const howToPlay = [
+    {
+        header: 'How To Play: Standard Rules',
+        body: [
+            {
+                section: 'The Basics',
+                text: ```Triple Triad is a card game played on a 3x3 grid between two players.
+                Each player has five cards, and the objective is to control as many spots on
+                the board as possible. The game begins with a random selection of which player
+                will go first.```,
+            },
+            {
+                section: 'Make Your Move',
+                text: ```During their turn, players can choose an unoccupied spot on the board
+                to place one of their cards. Each card has a color representing the player it
+                belongs to and four values placed on its edges. When a player places a card
+                adjacent to an opponent's card, the values facing each other are compared. If
+                the placing player's value is higher, the opponent's card is captured and
+                changes color to represent the capturing player. Players can only capture cards
+                on their own turn.```,
+            },
+            {
+                section: 'Capture the Win',
+                text: ```The game continues until all spots on the board are filled. At that
+                point, the player with the most cards in their color, including the unplayed
+                card, wins. If both players have an equal number of cards, the game is
+                considered a draw.```,
+            },
+        ],
+        images: [],
+    },
+    {
+        header: 'Multiple Rounds',
+        body: [
+            {
+                section: 'Extending the battle',
+                text: ```As users progress, they will encounter stronger opponents who possess
+                a greater arsenal of cards. These battles consist of multiple rounds, determined
+                by the round count specified for the opponent. Each round follows the same rules
+                as a standard battle, but with an additional scoring system.```,
+            },
+            {
+                section: 'Keeping Score',
+                text: ```Each player will have a counter to keep track of how many rounds they
+                have won. The battle concludes either when all the rounds are completed or when
+                one player has won the majority of the rounds. If both players have an equal
+                number of round wins by the end of the battle, it results in a draw.```,
+            },
+            {
+                section: 'Greater Knowledge',
+                text: ```These battles challenge players to showcase their skills and strategic
+                thinking as they strive to outscore their opponents and emerge victorious. With
+                greater struggle comes greater rewards!```,
+            },
+        ],
+    },
+]
+
+export default howToPlay
--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,3 +1,4 @@
 export { default as navlinks } from './navlinks'
 export { default as onboardingStages } from './onboardingStages'
+export { default as howToPlay } from './howToPlay'
 export { default as userLevels } from './userLevels'

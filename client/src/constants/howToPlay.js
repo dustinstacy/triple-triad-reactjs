6bca3946275dc2cb1092c21706964262f00f7cb2
Dustin Stacy <dustin.stacy@outlook.com>
2023-06-04 20:12:36 -0400

    Add howToPlay text to Rules page

--- client/src/constants/howToPlay.js
+++ client/src/constants/howToPlay.js
@@ -3,28 +3,63 @@ const howToPlay = [
         header: 'How To Play: Standard Rules',
         body: [
             {
-                section: 'The Basics',
-                text: ```Triple Triad is a card game played on a 3x3 grid between two players.
-                Each player has five cards, and the objective is to control as many spots on
-                the board as possible. The game begins with a random selection of which player
-                will go first.```,
+                title: 'The Basics -',
+                content: [
+                    {
+                        line: `Triple Triad is a card game played on a 3x3 grid between two players.`,
+                    },
+                    {
+                        line: `Each player has five cards, and the objective is to control as many spots on
+                the board as possible.`,
+                    },
+                    {
+                        line: `The game begins with a random selection of which player
+                will go first.`,
+                    },
+                ],
             },
             {
-                section: 'Make Your Move',
-                text: ```During their turn, players can choose an unoccupied spot on the board
-                to place one of their cards. Each card has a color representing the player it
-                belongs to and four values placed on its edges. When a player places a card
-                adjacent to an opponent's card, the values facing each other are compared. If
+                title: 'Make Your Move -',
+                content: [
+                    {
+                        line: `During their turn, players can choose an unoccupied spot on the board
+                to place one of their cards.`,
+                    },
+                    {
+                        line: `Each card has a color representing the player it
+                belongs to and four values placed on its edges.`,
+                    },
+                    {
+                        line: `When a player places a card
+                adjacent to an opponent's card, the values facing each other are compared.`,
+                    },
+                    {
+                        line: `If
                 the placing player's value is higher, the opponent's card is captured and
-                changes color to represent the capturing player. Players can only capture cards
-                on their own turn.```,
+                changes color to represent the capturing player.`,
+                    },
+                    {
+                        line: `Players can only capture cards
+                on their own turn.`,
+                    },
+                ],
             },
             {
-                section: 'Capture the Win',
-                text: ```The game continues until all spots on the board are filled. At that
+                title: 'Capture the Win -',
+                content: [
+                    {
+                        line: `The game continues until all spots on the board are filled.`,
+                    },
+                    {
+                        line: `At that
                 point, the player with the most cards in their color, including the unplayed
-                card, wins. If both players have an equal number of cards, the game is
-                considered a draw.```,
+                card, wins.`,
+                    },
+                    {
+                        line: `If both players have an equal number of cards, the game is
+                considered a draw.`,
+                    },
+                ],
             },
         ],
         images: [],
@@ -33,24 +68,51 @@ const howToPlay = [
         header: 'Multiple Rounds',
         body: [
             {
-                section: 'Extending the battle',
-                text: ```As users progress, they will encounter stronger opponents who possess
-                a greater arsenal of cards. These battles consist of multiple rounds, determined
-                by the round count specified for the opponent. Each round follows the same rules
-                as a standard battle, but with an additional scoring system.```,
+                title: 'Extending the battle -',
+                content: [
+                    {
+                        line: `As users progress, they will encounter stronger opponents who possess
+                a greater arsenal of cards.`,
+                    },
+                    {
+                        line: `These battles consist of multiple rounds, determined
+                by the round count specified for the opponent.`,
+                    },
+                    {
+                        line: `Each round follows the same rules
+                as a standard battle, but with an additional scoring system.`,
+                    },
+                ],
             },
             {
-                section: 'Keeping Score',
-                text: ```Each player will have a counter to keep track of how many rounds they
-                have won. The battle concludes either when all the rounds are completed or when
-                one player has won the majority of the rounds. If both players have an equal
-                number of round wins by the end of the battle, it results in a draw.```,
+                title: 'Keeping Score -',
+                content: [
+                    {
+                        line: `Each player will have a counter to keep track of how many rounds they
+                have won.`,
+                    },
+                    {
+                        line: `The battle concludes either when all the rounds are completed or when
+                one player has won the majority of the rounds.`,
+                    },
+                    {
+                        line: `If both players have an equal
+                number of round wins by the end of the battle, it results in a draw.`,
+                    },
+                ],
             },
             {
-                section: 'Greater Knowledge',
-                text: ```These battles challenge players to showcase their skills and strategic
-                thinking as they strive to outscore their opponents and emerge victorious. With
-                greater struggle comes greater rewards!```,
+                title: 'Greater Knowledge -',
+                content: [
+                    {
+                        line: `These battles challenge players to showcase their skills and strategic
+                thinking as they strive to outscore their opponents and emerge victorious.`,
+                    },
+                    {
+                        line: `With
+                greater struggle comes greater rewards!`,
+                    },
+                ],
             },
         ],
     },
--- client/src/pages/Rules/Rules.jsx
+++ client/src/pages/Rules/Rules.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import { useGlobalContext } from '../../context/GlobalContext'
-import { ProductTour } from '../../components'
+import { useGlobalContext } from '@context'
+import { ProductTour } from '@components'
+import { howToPlay } from '@constants'
 
 import './Rules.scss'
 
@@ -12,10 +13,25 @@ const Rules = () => {
     return (
         <div className='rules page'>
             {stage === 3 && <ProductTour step={5} />}
-            <div className='panel'>
-                <h1>HoW To PLay</h1>
-                <p>- This is how you play</p>
-            </div>
+            {howToPlay.map((panel) => (
+                <div key={panel.header} className='panel'>
+                    <h1>{panel.header}</h1>
+                    <div className='panel-body'>
+                        {panel.body.map((section) => (
+                            <div key={section.title} className='panel-section'>
+                                <h2>{section.title}</h2>
+                                <ul className='panel-section-content'>
+                                    {section.content.map((content, i) => (
+                                        <li key={content.line.slice(0, 10)}>
+                                            {content.line}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }

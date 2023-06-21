dd45b60922e5e5db20b3dfed22c2e2b435847553
Dustin Stacy <dustin.stacy@outlook.com>
2023-06-21 16:53:45 -0400

    Add file structure for ProductTour to Onboarding migration

---
+++ client/src/components/Onboarding/components/CompletionReward/CompletionReward.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './CompletionReward.scss'
+
+const CompletionReward = () => {
+    return <div>CompletionReward</div>
+}
+
+export default CompletionReward
---
+++ client/src/components/Onboarding/components/HowToBuildADeck/HowToBuildADeck.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './HowToBuildADeck.scss'
+
+const HowToBuildADeck = () => {
+    return <div>HowToBuildADeck</div>
+}
+
+export default HowToBuildADeck
